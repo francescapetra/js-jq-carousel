6cd6b2b841e9a1c4452f9c346aa2869680ed43a8
francescapetra <francescalaurapetra@gmail.com>
2021-03-12 14:43:05 +0100

    create variabili e funzioni per i due eventi click

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 $(function(){
+//alert("debug");
+// creo variabili esterne per i miei due elementi click
+var previous = $(".prev");
+var next = $(".next");
+//click al prev
+previous.click(
+  function(){
+    var imgShow = $(".images img.active");
+    var imgLast = $(".images img.last");
 
+    imgShow.removeClass("active");
 
+      if (imgShow.hasClass("first") == true) {
+        imgLast.addClass("active");
+      }else {
+        imgShow.prev().addClass("active");
+      }
+  }
+);
+// click al next
+next.click(
+  function(){
+    var imgShow = $(".images img.active");
+    var imgLast = $(".images img.last");
 
+    imgShow.removeClass("active");
+
+      if (imgShow.hasClass("first") == true) {
+        imgLast.addClass("active");
+      }else {
+        imgShow.prev().addClass("active");
+      }
+  }
+);
 
 
 
