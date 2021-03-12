39504bd495b540629d563b918652ee825c447ade
francescapetra <francescalaurapetra@gmail.com>
2021-03-12 15:05:38 +0100

    corrette funzioni last/first

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ $(function(){
 // creo variabili esterne per i miei due elementi click
 var previous = $(".prev");
 var next = $(".next");
+var bullet = $(".nav .fa-circle")
 //click al prev
 previous.click(
   function(){
     var imgShow = $(".images img.active");
     var imgLast = $(".images img.last");
+    var imgFirst = $(".images img.first");
 
     imgShow.removeClass("active");
 
       if (imgShow.hasClass("first") == true) {
         imgLast.addClass("active");
+
       }else {
         imgShow.prev().addClass("active");
       }
@@ -23,18 +26,30 @@ next.click(
   function(){
     var imgShow = $(".images img.active");
     var imgLast = $(".images img.last");
+    var imgFirst = $(".images img.first");
 
     imgShow.removeClass("active");
 
-      if (imgShow.hasClass("first") == true) {
-        imgLast.addClass("active");
+      if (imgShow.hasClass("last") == true) {
+        imgFirst.addClass("active");
       }else {
         imgShow.prev().addClass("active");
       }
   }
 );
 
-
+// bullet.click(
+//   function(){
+//     var imgShow = $(".images img.active");
+//     var imgLast = $(".images img.last");
+//
+//     bullet.removeClass("active");
+//
+//       if (bullet.hasClass("first") == true) {
+//         bullet.addClass("active");
+//   }
+// }
+// );
 
 
 
